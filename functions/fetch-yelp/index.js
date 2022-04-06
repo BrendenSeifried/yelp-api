65af93d024a1741ff41168ccf754825afc5f2007
BrendenSeifried <brendenseifried@gmail.com>
2022-04-06 12:33:36 -0700

    did the index for fetch-yelp but it broke think i need to add to Apps

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -2,6 +2,28 @@ const fetch = require('node-fetch');
 require('dotenv').config({ path: `.env.development.local` });
 
 const handler = async (event) => {
+  const zip = event.queryStringParameters.zip;
+  console.log(zip);
+  try {
+    const resp = await fetch(`https://api.yelp.com/v3/businesses/`, {
+      header: {
+        Authorization: `Bearer ${process.env.REACT_APP_YELP_KEY}`,
+      },
+    });
+    const data = await resp.json();
+    const json = JSON.stringify(data);
+
+    return {
+      statusCode: 200,
+      body: json,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      satusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
+  }
   // add code here to fetch data from yelp API
   // be sure to include the parameters from event.queryStringParameters
 };
